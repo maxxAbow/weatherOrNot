09fbaa52c3fde8aff7ea241d456d69430b208239
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-02-09 11:30:33 -0500

    modulized routes for index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
-// Import our modular routers for /tips and /feedback
+// Import our modular routers for /login, /profile, and /scheduler
 const loginRouter = require('./login');
 const profileRouter = require('./profile');
 const schedulerkRouter = require('./scheduler');
 
-
 const app = express();
+
+app.use('/login', loginRouter);
+app.use('/profile', profileRouter);
+app.use('/scheduler', schedulerkRouter);
