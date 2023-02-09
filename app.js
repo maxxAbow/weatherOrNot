1faf6430e87291e7807ffec7a319ea36d73629a0
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-02-09 09:15:27 -0500

    refactored app.js, and added PORT

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const sequalize = require('sequalize');
+const sequalize = require('./config/connection');
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+
+app.listen(PORT, () => console.log(`Connected via http://localhost:${PORT}`))
