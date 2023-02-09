8423b88587d103c7fabdf91fb9bcd8acc7a42605
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-02-09 13:56:23 -0500

    refactored scheduler.js

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -1,10 +1,8 @@
 const scheduler = require('express').Router();
-const path = require('path');
 
-// scheduler.get('/', (req,res) => res.sendFile(`${__dirname}../views/scheduler.html`))
-scheduler.get('/scheduler', (req,res) => {
-    console.log(req.method);
-    res.sendFile(path.join(__dirname, '../views/scheduler.html'));
+scheduler.get('/', (req, res) => {
+  console.log(req.method);
+  res.send('Scheduler page');
 });
 
 module.exports = scheduler;
