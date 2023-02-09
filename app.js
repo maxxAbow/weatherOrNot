69daf1e41cc7d5e2cbb78c623170ad185250841f
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-02-09 15:19:57 -0500

    moved files in directory, changed href in login.html

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const sequalize = require('./config/connection');
-const path = require('path');
+// const path = require('path');
 const PORT = process.env.PORT || 3001;
 const loginRouter = require('./routes/login')
 const profileRouter = require('./routes/profile');
 
 const app = express();
 
-// app.use(express.static('public'))
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Route for home page
 app.get('/', (req, res) => res.send('Home Page'));
