eaf57973e2ddf86a8a3f31a6b06c4f9785b618e2
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-02-09 09:29:12 -0500

    Created route for login page

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const sequalize = require('./config/connection');
 
 const app = express();
 
+app.use(express.static('views'))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const PORT = process.env.PORT || 3001;
 
+// Route for home/login page
+app.get('/', (req, res) => res.sendFile(`${__dirname}/views/login.html`))
 
 app.listen(PORT, () => console.log(`Connected via http://localhost:${PORT}`))
