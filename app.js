0996138be12e5949c890b4065a9171d183c8c5bd
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-02-09 09:02:50 -0500

    add middleware

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
-const sequalize = require('sequalize')
+const sequalize = require('sequalize');
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 const app = express();
