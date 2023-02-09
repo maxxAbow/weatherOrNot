ceeafbf3e7987415bd0cb24fd812bc81dab0bce0
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-02-09 13:52:58 -0500

    refactored app.js, added routes for modulized routes

--- app.js
+++ app.js
@@ -2,24 +2,18 @@ const express = require('express');
 const sequalize = require('./config/connection');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
+const profileRouter = require('./routes/profile');
 
 const app = express();
 
+// app.use(express.static('public'))
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('views'))
-
-
-// Route for home/login page
-app.get('/', (req, res) => res.sendFile(`${__dirname}/views/index.html`)) //rename this file to index.html
+// Route for home page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
 
 // Route for profile page
-// app.get('/profile', (req,res) => res.sendFile(`${__dirname}/views/profile.html`))
-app.get('/profile', require('./routes/profile'))
+app.use('/profile', profileRouter);
 
-// Route for scheduler page
-app.get('/scheduler', require('./routes/scheduler'))
-
-// app.use('/login', require('./routes/login'))
-app.listen(PORT, () => console.log(`Connected via http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Connected via http://localhost:${PORT}`));
--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,9 @@
-const profile = require('express').Router();
-const path = require('path');
+const express = require('express');
+const schedulerRouter = require('./scheduler');
+
+const profile = express.Router();
 
 profile.get('/profile', (req,res) => res.sendFile(path.join(__dirname,'../views/profile.html')));
 
+profile.use('/scheduler', require('./scheduler'));
 module.exports = profile;
