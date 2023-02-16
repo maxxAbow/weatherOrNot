9a1f4462605915d51c4f7529c2c2d2bf3844e78e
David Mackey <davidmackey92@gmail.com>
2023-02-16 17:12:35 -0600

    yo

--- public/js/login.js
+++ public/js/login.js
@@ -1,34 +1,37 @@
-const signinForm = document.querySelector("#signinForm");
-const signupForm = document.querySelector("#signupForm");
-const emailInput = document.querySelector("#emailInput");
-const passwordInput = document.querySelector("#passwordInput");
-const signInButton = document.querySelector("#signInButton");
-const displayNameInput = document.querySelector("#display-name-input")
-const signUpEmailInput = document.querySelector("#sign-up-email-input")
-const signUpPasswordInput = document.querySelector("#sign-up-password-input")
-const signUpButton = document.querySelector("#sign-up-button")
+// declaring consts and querySelectors
+const signinForm = document.querySelector("#signinForm"); // id
+const signupForm = document.querySelector("#signupForm"); // id
+const emailInput = document.querySelector("#emailInput"); // id
+const passwordInput = document.querySelector("#passwordInput"); // id
+const signInButton = document.querySelector("#signInButton"); // id
+const displayNameInput = document.querySelector("#display-name-input"); // id
+const signUpEmailInput = document.querySelector("#sign-up-email-input"); // id
+const signUpPasswordInput = document.querySelector("#sign-up-password-input"); // id
+const signUpButton = document.querySelector("#sign-up-button"); // id
 
+// adding a click event listener to the element with Id of 'showSignup'
 const showSignup = document.querySelector("#showSignup").addEventListener('click', () => {
-  if (signupForm.style.display === "none") {
-    signupForm.style.display = "block";
-    signinForm.style.display = "none";
-  } else {
-    signupForm.style.display === "none";
+  if (signupForm.style.display === "none") { // 
+    signupForm.style.display = "block"; // 
+    signinForm.style.display = "none"; // 
+  } else { // 
+    signupForm.style.display = "none"; // 
   }
 });
+// adding a click event listener to the element with Id of 'showSignin'
 const showSignin = document.querySelector("#showSignin").addEventListener('click', () => {
-  if (signinForm.style.display === "none") {
-    signinForm.style.display = "block";
-    signupForm.style.display = "none";
-  } else {
-    signinForm.style.display = "none";
+  if (signinForm.style.display === "none") { // 
+    signinForm.style.display = "block"; // 
+    signupForm.style.display = "none"; // 
+  } else { // 
+    signinForm.style.display = "none"; // 
   }
 });
-
+// adding click event listener to the element with ID of 'signInButton'
 signInButton.addEventListener('click', async (e) => {
-  e.preventDefault()
-  const emailInputValue = emailInput.value
-  const passwordInputValue = passwordInput.value
+  e.preventDefault();
+  const emailInputValue = emailInput.value;
+  const passwordInputValue = passwordInput.value;
   const response = await fetch('/api/users/login', {
     method: 'POST',
     headers: {
@@ -38,18 +41,18 @@ signInButton.addEventListener('click', async (e) => {
       emailAddress: emailInputValue,
       password: passwordInputValue
     })
-  })
-  const parsedResponse = await response.json()
-  localStorage.setItem('activeUser', JSON.stringify(parsedResponse))
-  window.location.replace('/scheduler')
-})
-
+  });
+  const parsedResponse = await response.json();
+  localStorage.setItem('activeUser', JSON.stringify(parsedResponse));
+  window.location.replace('/scheduler');
+});
+// adding a click event listener to element Id 'signUpButton'
 signUpButton.addEventListener('click', async (e) => {
-  e.preventDefault()
-  
-  const emailInputValue = signUpEmailInput.value
-  const passwordInputValue = signUpPasswordInput.value
-  const displayNameValue = displayNameInput.value
+  e.preventDefault();
+
+  const emailInputValue = signUpEmailInput.value;
+  const passwordInputValue = signUpPasswordInput.value;
+  const displayNameValue = displayNameInput.value;
   const response = await fetch('/api/users/', {
     method: 'POST',
     headers: {
@@ -59,13 +62,10 @@ signUpButton.addEventListener('click', async (e) => {
       emailAddress: emailInputValue,
       password: passwordInputValue,
       displayName: displayNameValue,
-    
+
     })
-  })
-  const parsedResponse = await response.json()
-  localStorage.setItem('activeUser', JSON.stringify(parsedResponse))
-  window.location.replace('/scheduler')
-})
-
-
-
+  });
+  const parsedResponse = await response.json();
+  localStorage.setItem('activeUser', JSON.stringify(parsedResponse));
+  window.location.replace('/scheduler');
+});
